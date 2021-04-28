f0c8e30343d1052840b1a8864949c02335092f8f
Avhijit Nair <avhijit.18bce7040@vitap.ac.in>
2021-04-28 23:17:33 +0530

    Update Authentication.js

--- healthcare/src/components/Authentication.js
+++ healthcare/src/components/Authentication.js
@@ -4,18 +4,57 @@ import '../App.css';
 import axios from 'axios';
 
 class Authentication extends Component{
+	constructor(props) {
+    super(props);
+    this.state = {
+      uname: '',
+      pname: '',
+    };
+  }
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+  }
 	render(){
-		return(
-			<form>
-			Name:<br></br>
-			<input type = "text"></input><br></br><br></br>
-			Password:<br></br>
-			<input type = "password"></input><br></br><br></br>
-			<input type = "submit"></input>
-			<input type = "reset"></input><br></br><br></br>
-			<button id="c"><Link to="/">Back</Link></button>
-			</form>
-			);
-	}
-}
+    return(
+      <div id="loginform">
+        <FormHeader title="Login" />
+        <Form uname = {this.state.uname} pname={this.state.pname} event={this.myChangeHandler}/>
+        
+      </div>
+    )
+  }
+  }
+  
+const FormHeader = props => (
+  <h2 id="headerTitle">{props.title}</h2>
+);
+
+
+const Form = props => (
+ <div>
+   <FormInput description="Username" placeholder="Enter your username" type="text" name="uname" event={props.event}/>
+   <FormInput description="Password" placeholder="Enter your password" type="password" name="pname" event={props.event}/>
+   <FormButton title="Log In"/>
+   <div id="alternativeLogin">
+    <label>Or sign up with:</label>
+    </div>
+   <FormButton title="Sign Up" link="/Registration"/>
+ </div>
+);
+
+const FormButton = props => (
+<div id="button" class="row">
+  <button><Link to={props.link}>{props.title}</Link></button>
+</div>
+);
+
+const FormInput = props => (
+<div class="row">
+  <label>{props.description}</label>
+  <input type={props.type} placeholder={props.placeholder} name ={props.name} onChange={props.event}/>
+</div>  
+);
+
 export default Authentication;
